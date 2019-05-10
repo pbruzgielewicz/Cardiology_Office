93924a4312c85d36f029224b4b5c090f683d59c8
pbruzgielewicz <p.bruzgielewicz@gmail.com>
2019-05-10 23:03:54 +0200

    sql file changed

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,3 +1,1 @@
-INSERT INTO patients (firstName, lastName, pesel, adress) VALUES ('Jan', 'Kowalski', '90081344990','Oswiecim ');
-INSERT INTO physicalExaminations (eyes, growth,pressure, pulse, weight) VALUES ('niebieskie', '180 cm', '120/90','60','90');
-INSERT INTO addictionalExaminationTypes (name) VALUES ('Ekg');
+INSERT INTO patients (firstName, lastName, pesel, adress) VALUES ('Jan', 'Kowalski', '89020210257','Katowice ');
